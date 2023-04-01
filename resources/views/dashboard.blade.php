65069e9e6da48b2a0d9bca279f479c8acc025149
Weerapat Ketwang <weerapatkw@Macbooks-MacBook-Air-3.local>
2023-04-01 14:45:47 +0700

    dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('ZBuyer') }}
+            {{ __('Headder') }}
         </h2>
     </x-slot>
 
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-appz-layout>
